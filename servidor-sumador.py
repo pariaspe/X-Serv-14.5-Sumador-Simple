b6dc424f655b4c5103bee32bac3d34dc6407103b
pariaspe <pedroariasperez96@gamil.com>
2018-02-25 20:58:12 +0100

    Version mejorada tras la clase

--- servidor-sumador.py
+++ servidor-sumador.py
@@ -10,6 +10,25 @@ SAT , Universidad Rey Juan Carlos
 import socket
 import calculadora
 
+def analize(request):
+    resource = request.split()[1]
+    print(resource)
+    _, num1, operacion, num2 = resource.split('/')
+    return num1, operacion, num2
+
+def compute(num1, operacion, num2):
+    op = num1 + " " + operacion + " " + num2 + " = "
+    op += str(calculadora.operations[operacion](int(num1), int(num2)))
+    return op
+
+def build_answer(op):
+    html_answer = '<html><body><h1>Bienvenido a la calculadora web.</h1>'
+    html_answer += '<p>La operacion a realizar es:\r\t' + op
+    html_answer += '</p></body>'
+
+    answer = "HTTP/1.1 200 OK\r\n\r\n" + html_answer + '\r\n'
+    return answer
+
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 mySocket.bind(('0.0.0.0', 1234))
@@ -23,15 +42,13 @@ try:
         print('Request received:')
         request = str(recvSocket.recv(1024), 'utf-8')
         print(request)
-        resource = request.split()[1]
-        print(resource)
-        _, num1, operacion, num2 = resource.split('/')
+
+        num1, operacion, num2 = analize(request)
         print('Answering back...')
 
         # Building Answer
         try:
-            op = num1 + " " + operacion + " " + num2 + " = "
-            op += str(calculadora.operations[operacion](int(num1), int(num2)))
+            op = compute(num1, operacion, num2)
         except KeyError:
             op = "Operacion no permitida(" + operacion + ")"
         except ZeroDivisionError:
@@ -39,13 +56,9 @@ try:
         except:
             op = "Error"
 
-        html_answer = '<html><body><h1>Bienvenido a la calculadora web.</h1>'
-        html_answer += '<p>La operacion a realizar es:\r\t' + op
-        html_answer += '</p></body>'
-
+        answer = build_answer(op)
         # Answering
-        recvSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n" +
-                        html_answer + '\r\n', 'utf-8'))
+        recvSocket.send(bytes(answer, 'utf-8'))
         recvSocket.close()
 except KeyboardInterrupt:
     print("Closing binded socket")
